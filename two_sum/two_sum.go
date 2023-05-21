a421e83a3f3c5378bf02ba33c56135b3ff6f3f68
capahdan <2081029@unai.edu>
2023-05-21 16:22:51 +0700

    selesai

--- two_sum/two_sum.go
+++ two_sum/two_sum.go
@@ -1,7 +1,13 @@
 package two_sum
 
 func TwoSum(nums []int, target int) []int {
-	data := []int{0, 0}
-
-	return data
+	data := make(map[int]int)
+	for i := range nums {
+		searced := target - nums[i]
+		if index, exist := data[searced]; exist {
+			return []int{index, i}
+		}
+		data[nums[i]] = i
+	}
+	return nil
 }
