095cef716b90ea40e7a084a1e1fcb1a694f41b44
daniel <danielcapah777@gmail.com>
2023-10-18 07:07:29 +0700

    feat : day 12 of doing 150 top interview leetcode

---
+++ 219.contains_duplicates2/contain_duplicate.go
@@ -0,0 +1,20 @@
+package word_pattern
+
+import "fmt"
+
+func ContainsNearbyDuplicate(nums []int, k int) bool {
+	// map [int]int ,
+
+	numMap := make(map[int]int)
+
+	for index := range nums {
+		v, ok := numMap[nums[index]]
+		fmt.Println(v, ok)
+		if v, ok := numMap[nums[index]]; ok && index-v <= k {
+			return true
+		}
+		numMap[nums[index]] = index
+	}
+
+	return false
+}
---
+++ 219.contains_duplicates2/contain_duplicate_test.go
@@ -0,0 +1,54 @@
+package word_pattern_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	. "github.com/capahdan/coding-interview/219.contains_duplicates2"
+)
+
+type TestTable struct {
+	name     string
+	input    input
+	expected bool
+}
+type input struct {
+	nums []int
+	k    int
+}
+
+func TestContainDuplicate(t *testing.T) {
+	testTable := []TestTable{
+		{
+			name: "test1",
+			input: input{
+				nums: []int{1, 2, 3, 1},
+				k:    3,
+			},
+			expected: true,
+		},
+		{
+			name: "test2",
+			input: input{
+				nums: []int{1, 0, 1, 1},
+				k:    1,
+			},
+			expected: true,
+		},
+		{
+			name: "test3",
+			input: input{
+				nums: []int{1, 2, 3, 1, 2, 3},
+				k:    2,
+			},
+			expected: false,
+		},
+	}
+
+	for _, tt := range testTable {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equal(t, tt.expected, ContainsNearbyDuplicate(tt.input.nums, tt.input.k), tt.expected)
+		})
+	}
+}
